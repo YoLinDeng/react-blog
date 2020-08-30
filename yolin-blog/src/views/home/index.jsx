daba6721a61aaa6467c9328d3ac12105dc715e40
YoLinDeng <1091842103@qq.com>
2020-08-30 16:18:27 +0800

    首页布局完成

--- yolin-blog/src/views/home/index.jsx
+++ yolin-blog/src/views/home/index.jsx
@@ -1,5 +1,8 @@
 import './index.scss'
 import React, { Component }  from 'react'
+import { Tag, Pagination  } from 'antd';
+import { ClockCircleOutlined, AppstoreOutlined, TagOutlined } from '@ant-design/icons';
+
 class Home extends Component {
   constructor(props) {
     super(props)
@@ -8,14 +11,87 @@ class Home extends Component {
       articleList: [
         {
           title: 'egg.js+mongoose实现二级评论',
-          detail: '',
+          desc: '关于egg.js+mongoose实现二级评论的描述，egg.js+mongoose实现二级评论egg.js+mongoose实现二级评论',
+          time: '2020-8-30',
+          type: '前端,后端,移动端',
+          label: 'vue,react,eggg,ts'
+        },
+        {
+          title: 'egg.js+mongoose实现二级评论',
+          desc: 'egg.js+mongoose实现二级评论egg.js+mongoose实现二级评论egg.js+mongoose实现二级评论egg.js+mongoose实现二级评论egg.js+mongoose实现二级评论',
+          time: '2020-8-30',
+          type: '前端,后端,移动端',
+          label: 'vue,react,eggg,ts,js'
         }
-      ]
+      ],
     }
+  }
+  splitString(str) {
+    return str.split(',')
+  }
+  onChangePagination(page, pageSize) {
+    console.log(page, pageSize)
   }
   render() {
     return(
       <div className="home-wrapper">
+        {
+          this.state.articleList.map((item, index) => {
+            return(
+              <div className="article-item" key={index}>
+                <div className="top">
+                  <a className="title" href="/article/detail">{item.title}</a>
+                  <div className="time-box">
+                  <ClockCircleOutlined />
+                  <span className="text">{item.time}</span>
+                  </div>
+                </div>
+                <div className="center-box">
+                  <a className="desc" href="/article/detail">
+                    {item.desc}
+                  </a>
+                </div>
+                <div className="footer-box">
+                  <div className="container">
+                    <AppstoreOutlined className="icon"/>
+                    <span className="title">分类：</span>
+                    {
+                      this.splitString(item.type).map((tItem, tIndex) => {
+                        return(
+                        <Tag color="#87d068" key={tIndex}>
+                          <a className="item" href="/type">{tItem}</a>
+                        </Tag>
+                        )
+                      })
+                    }
+                  </div>
+                  <div className="container">
+                  <TagOutlined className="icon"/>
+                    <span className="title">标签：</span>
+                    {
+                      this.splitString(item.label).map((lItem, lIndex) => {
+                        return(
+                        <Tag color="#2db7f5" key={lIndex}>
+                          <a className="item" href="/label">{lItem}</a>
+                        </Tag>
+                        )
+                      })
+                    }
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+        <div className="pagination-box">
+          <Pagination
+            total={85}
+            showSizeChanger
+            showQuickJumper
+            onChange={this.onChangePagination}
+            showTotal={total => `共 ${total} 条`}
+          />
+        </div>
       </div>
     )
   }
