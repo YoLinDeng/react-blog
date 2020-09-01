d4908fa7b517af6fc5e113ec6fdf49b08dae039c
YoLinDeng <yonglingdeng@infinite.net.cn>
2020-09-01 18:26:39 +0800

    修改学习界面

--- yolin-blog/src/views/study/index.jsx
+++ yolin-blog/src/views/study/index.jsx
@@ -4,13 +4,70 @@ class Study extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      docList: [
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        },
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        },
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        },
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        },
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        },
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        },
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        },
+        {
+          image: require('@/assets/images/vue-logo.png'),
+          url: 'https://cn.vuejs.org/',
+          title: 'vue.js'
+        }
+      ]
     };
+    this.goToDoc = this.goToDoc.bind(this)
+  }
+  goToDoc(url) {
+    window.location.href = url
   }
   render() {
     return (
       <div className="study-wrapper">
-        学习
+        <div className="doc-list">
+          <p className="top-title">常用文档</p>
+          {
+            this.state.docList.map((item,index) => {
+              return(
+                <div className="list-item" key={index} onClick={() => this.goToDoc(item.url)}>
+                  <img src={item.image} alt="" className="item-image"/>
+                  <div className="item-title">{item.title}</div>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
