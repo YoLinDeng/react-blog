945e5ec5c6bee7445e5db2298885587d6d478b89
YoLinDeng <1091842103@qq.com>
2020-09-07 00:35:16 +0800

    修改导航组件

--- yolin-blog/src/components/Header/index.jsx
+++ yolin-blog/src/components/Header/index.jsx
@@ -20,7 +20,7 @@ class Header extends Component {
   componentDidMount = () => {
     let moren = this.props.location.pathname
     let text = moren.substring(moren.lastIndexOf('/') + 1, moren.length)
-    !['home', 'study', 'type', 'label', 'about', 'search'].includes(text) && (text = 'home')
+    !['home', 'study', 'photos', 'archives', 'about', 'search'].includes(text) && (text = 'home')
     this.setState({
       current: text
     })
